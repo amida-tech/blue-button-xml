e74f2e0eded4a49536c0b293a45ed44c92b45a0e
Marc Sylvestre <marc.sylvestre@manhydra.com>
2022-04-15 14:03:32 -0400

    Patch Update: 1.8.3
    * Upgraded dev dependencies
    * Converted tests and coverages to jest

--- browser/lib/xml.js
+++ browser/lib/xml.js
@@ -1,78 +1,78 @@
 "use strict";
 
 exports.parse = function (src) {
-    var xml;
+  var xml;
 
-    if (typeof src === "string") {
-        xml = new DOMParser().parseFromString(src, "text/xml");
-    } else if (typeof src === "object" && src.constructor === Document) {
-        xml = src;
-    } else {
-        throw "Unrecognized document type " + typeof src;
-    }
+  if (typeof src === "string") {
+    xml = new DOMParser().parseFromString(src, "text/xml");
+  } else if (typeof src === "object" && src.constructor === Document) {
+    xml = src;
+  } else {
+    throw "Unrecognized document type " + typeof src;
+  }
 
-    return xml;
+  return xml;
 };
 
 exports.leafNodeValue = function (node) {
-    var ret = null;
+  var ret = null;
 
-    if (node.text) {
-        if (typeof node.text === "string") {
-            ret = node.text;
-        }
-        if (typeof node.text === "function") {
-            ret = node.text();
-        }
-    } else if (node.value) { // attribute
-        if (typeof node.value === "string") {
-            ret = node.value;
-        }
-        if (typeof node.value === "function") {
-            ret = node.value();
-        }
-    } else if (node.data) {
-        if (typeof node.data === "string") {
-            ret = node.data;
-        }
-        if (typeof node.data === "function") {
-            ret = node.data();
-        }
-    } else {
-        ret = "";
+  if (node.text) {
+    if (typeof node.text === "string") {
+      ret = node.text;
     }
+    if (typeof node.text === "function") {
+      ret = node.text();
+    }
+  } else if (node.value) { // attribute
+    if (typeof node.value === "string") {
+      ret = node.value;
+    }
+    if (typeof node.value === "function") {
+      ret = node.value();
+    }
+  } else if (node.data) {
+    if (typeof node.data === "string") {
+      ret = node.data;
+    }
+    if (typeof node.data === "function") {
+      ret = node.data();
+    }
+  } else {
+    ret = "";
+  }
 
-    //removes all linebreaks, tabs, and dedups whitespaces after
-    ret = ret.replace(/(\r\n|\n|\r|\t)/gm, " ");
-    ret = ret.replace(/\s+/g, ' ');
+  //removes all linebreaks, tabs, and dedups whitespaces after
+  ret = ret.replace(/(\r\n|\n|\r|\t)/gm, " ");
+  ret = ret.replace(/\s+/g, ' ');
 
-    //trim string
-    ret = ret.trim();
+  //trim string
+  ret = ret.trim();
 
-    return ret;
+  return ret;
 };
 
 exports.xpath = (function () {
-    var DEFAULT_NS = {
-        "h": "urn:hl7-org:v3",
-        "xsi": "http://www.w3.org/2001/XMLSchema-instance"
-    };
+  var DEFAULT_NS = {
+    "h": "urn:hl7-org:v3",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance"
+  };
 
-    return function (doc, p, ns) {
-        var r = [];
-        var a = doc.ownerDocument || doc;
-        var b = doc.ownerDocument ? doc : doc.documentElement;
-        var riter = a.evaluate(p, b, function (n) {
-            return (ns || DEFAULT_NS)[n] || null;
-        }, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
-        while (true) {
-            var tmp = riter.iterateNext();
-            if (tmp) {
-                r.push(tmp);
-            } else {
-                break;
-            }
-        }
-        return r;
-    };
+  return function (doc, p, ns) {
+    var r = [];
+    var a = doc.ownerDocument || doc;
+    var b = doc.ownerDocument ? doc : doc.documentElement;
+    var riter = a.evaluate(p, b, function (n) {
+      return (ns || DEFAULT_NS)[n] || null;
+    }, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+    while (true) {
+      var tmp = riter.iterateNext();
+      if (tmp) {
+        r.push(tmp);
+      } else {
+        break;
+      }
+    }
+    return r;
+  };
 })();
--- test/test-bbUtil.js
+++ test/test-bbUtil.js
@@ -1,56 +1,52 @@
 "use strict";
 
-var chai = require("chai");
-
 var bbUtil = require("../lib/bbUtil");
 
-var expect = chai.expect;
-
 describe('hl7 to/from iso8601 date/time conversion', function () {
-    var testCases = [{
-        hl7: '2012',
-        iso8601: "2012-01-01T00:00:00.000Z",
-        precision: 'year'
-    }, {
-        hl7: '201209',
-        iso8601: "2012-09-01T00:00:00.000Z",
-        precision: 'month'
-    }, {
-        hl7: '20120915',
-        iso8601: "2012-09-15T00:00:00.000Z",
-        precision: 'day'
-    }, {
-        hl7: '2012091521',
-        iso8601: "2012-09-15T00:00:00.000Z",
-        precision: 'day'
-    }, {
-        hl7: '201209152114',
-        iso8601: "2012-09-15T00:00:00.000Z",
-        precision: 'day'
-    }, {
-        hl7: '20120915211442',
-        iso8601: "2012-09-15T00:00:00.000Z",
-        precision: 'day'
-    }, {
-        hl7: '20120915211442+0200',
-        iso8601: "2012-09-15T19:14:42.000Z",
-        precision: 'second'
-    }, {
-        hl7: '20120915211442.123',
-        iso8601: "2012-09-15T00:00:00.000Z",
-        precision: 'day'
-    }, {
-        hl7: '20120915211442.123-0500',
-        iso8601: "2012-09-16T02:14:42.123Z",
-        precision: 'subsecond'
-    }];
+  var testCases = [{
+    hl7: '2012',
+    iso8601: "2012-01-01T00:00:00.000Z",
+    precision: 'year'
+  }, {
+    hl7: '201209',
+    iso8601: "2012-09-01T00:00:00.000Z",
+    precision: 'month'
+  }, {
+    hl7: '20120915',
+    iso8601: "2012-09-15T00:00:00.000Z",
+    precision: 'day'
+  }, {
+    hl7: '2012091521',
+    iso8601: "2012-09-15T00:00:00.000Z",
+    precision: 'day'
+  }, {
+    hl7: '201209152114',
+    iso8601: "2012-09-15T00:00:00.000Z",
+    precision: 'day'
+  }, {
+    hl7: '20120915211442',
+    iso8601: "2012-09-15T00:00:00.000Z",
+    precision: 'day'
+  }, {
+    hl7: '20120915211442+0200',
+    iso8601: "2012-09-15T19:14:42.000Z",
+    precision: 'second'
+  }, {
+    hl7: '20120915211442.123',
+    iso8601: "2012-09-15T00:00:00.000Z",
+    precision: 'day'
+  }, {
+    hl7: '20120915211442.123-0500',
+    iso8601: "2012-09-16T02:14:42.123Z",
+    precision: 'subsecond'
+  }];
 
-    testCases.forEach(function (testCase) {
-        it(testCase.precision, function () {
-            var iso = bbUtil.hl7ToISO(testCase.hl7);
-            expect(iso).to.equal(testCase.iso8601);
-            var precision = bbUtil.hl7ToPrecision(testCase.hl7);
-            expect(precision).to.equal(testCase.precision);
-        });
+  testCases.forEach(function (testCase) {
+    it(testCase.precision, function () {
+      var iso = bbUtil.hl7ToISO(testCase.hl7);
+      expect(iso).toBe(testCase.iso8601);
+      var precision = bbUtil.hl7ToPrecision(testCase.hl7);
+      expect(precision).toBe(testCase.precision);
     });
+  });
 });
